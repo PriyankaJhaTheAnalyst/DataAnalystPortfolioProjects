ef4704e1c56773eed8bbce30cddadd3cda7516e0
Priyanka Jha <priyanka.jha.social@gmail.com>
2021-07-15 12:04:14 +0530

    Update and rename COVID Portfolio SQL Script.sql to COVID Portfolio SQL - Data Exploration.sql

--- COVID Portfolio SQL - Data Exploration.sql
+++ COVID Portfolio SQL - Data Exploration.sql
@@ -1,3 +1,8 @@
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
 
 select *
 from coviddeaths_csv 
